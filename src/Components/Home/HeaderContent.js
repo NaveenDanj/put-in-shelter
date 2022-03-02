4aab38069190a9191511addd7bd3bc8df86ab2f7
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-03 03:13:05 +0530

    added router to help want component and create help want view

--- src/Components/Home/HeaderContent.js
+++ src/Components/Home/HeaderContent.js
@@ -1,25 +1,34 @@
 import React from 'react'
 import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
 
-function HeaderContent() {
-  return (
-      <>
-        <h1 style={styles.headerText}>SIMPLE / BEAUTIFUL</h1>
-        <h2 style={styles.subtitle }>LANDING PAGE</h2>
 
-        <div style={styles.content}>
-            <p style={{ marginTop : -5 ,}}>Aliquip dolor culpa ad officia tempor eu id do reprehenderit. Ex dolore ea anim laboris ea nostrud quis labore aliquip laboris incididunt. Officia exercitation nostrud laboris aute quis. Reprehenderit non veniam mollit sint dolor proident ad. Ea laboris duis sint duis consectetur cillum commodo aliqua ullamco.</p><br/>
-            
-            <div style={{ width : '100%' , textAlign : 'center' , display : 'flex' , justifyContent : 'space-around' , alignItems : 'center' , marginTop : 15}}>
-                <Button style={{ color: "#21b6ae", border : '1px solid white' }}  variant="outlined">I WANT HELP</Button>
-                <Button style={{ color: "#21b6ae", border : '1px solid white' }} variant="outlined">I WANT TO HELP</Button>
+function HeaderContent(context) {
+
+    let navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/helpwantsignup", { replace: true });
+    }
+    
+    return (
+        <>
+            <h1 style={styles.headerText}>SIMPLE / BEAUTIFUL</h1>
+            <h2 style={styles.subtitle }>LANDING PAGE</h2>
+
+            <div style={styles.content}>
+                <p style={{ marginTop : -5 ,}}>Aliquip dolor culpa ad officia tempor eu id do reprehenderit. Ex dolore ea anim laboris ea nostrud quis labore aliquip laboris incididunt. Officia exercitation nostrud laboris aute quis. Reprehenderit non veniam mollit sint dolor proident ad. Ea laboris duis sint duis consectetur cillum commodo aliqua ullamco.</p><br/>
+                
+                <div style={{ width : '100%' , textAlign : 'center' , display : 'flex' , justifyContent : 'space-around' , alignItems : 'center' , marginTop : 15}}>
+                    <Button onClick={() => handleClick() } style={{ color: "#21b6ae", border : '1px solid white' }}  variant="outlined">I WANT HELP</Button>
+                    <Button style={{ color: "#21b6ae", border : '1px solid white' }} variant="outlined">I WANT TO HELP</Button>
+                </div>
+
             </div>
 
-        </div>
 
-
-      </>
-  )
+        </>
+    )
 }
 
 
--- src/Views/HelpWantedRegister.js
+++ src/Views/HelpWantedRegister.js
@@ -1,9 +1,88 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
+import '../css/back.css'
+
 
 function HelpWantedRegister() {
-  return (
-    <div>HelpWantedRegister</div>
-  )
+
+    return (
+        <div className='gradient-background' style={ styles.mainContainer }>
+
+            <Card style={ styles.formContainer} variant="outlined">
+                
+                <h1 style={ styles.headerText }>Register</h1>
+
+                <div style={styles.fieldContainer}>
+
+                    <TextField
+                        error
+                        id="outlined-error-helper-text"
+                        label="Username"
+                        placeholder='Type your username'
+                        // helperText="Incorrect entry."
+                        size="small"
+
+                    /><br/>
+
+                    <TextField
+                        error
+                        id="outlined-error-helper-text"
+                        label="Password"
+                        type="password"
+                        placeholder='Type your password'
+                        // helperText="Incorrect entry."
+                        size="small"
+                        width="100%"
+
+                    />
+
+                    <Button class="gradient-background" style={{ marginTop : 15 , color : 'white' , fontSize : '1em' , padding : 10 , border : '0px' , borderRadius : 8 }}>Login</Button>
+
+                </div>
+
+
+
+            </Card>
+                
+
+        </div>
+    );
+
+}
+
+const styles = {
+
+    mainContainer : {
+        color : 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    },
+
+    formContainer : {
+        width : '60%',
+        maxWidth : '500px',
+        // height : '50vh',
+    },
+
+    headerText : {
+        textAlign: 'center',
+        fontWeight : 'bold',
+    },
+
+    fieldContainer : {
+        padding : 25,
+        display : 'flex',
+        flexDirection : 'column',
+        justifyContent : 'center',
+    }
+
 }
 
 export default HelpWantedRegister
