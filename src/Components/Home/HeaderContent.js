d78ebf150489ed2704fd5f9fd198b6bc2723861f
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-03 01:27:13 +0530

    added header content component

---
+++ src/Components/Home/HeaderContent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function HeaderContent() {
+  return (
+    <h1 style={styles.headerText}>Please help us!</h1>
+  )
+}
+
+
+const styles = {
+
+    headerText : {
+        fontSize: '4em',
+    }
+
+}
+
+export default HeaderContent
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import HeaderContent from '../Components/Home/HeaderContent'
+import '../css/Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='gradient-background' style={styles.mainContainer}>
+        <HeaderContent />
+    </div>
   )
+}
+
+const styles = {
+
+    mainContainer: {
+        color : 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    }
+
 }
 
 export default Home
