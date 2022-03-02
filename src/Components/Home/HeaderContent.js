9360ede72105d21049551010ce0c195e8e3f487c
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-03 01:50:58 +0530

    completed header content component

--- src/Components/Home/HeaderContent.js
+++ src/Components/Home/HeaderContent.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Button from '@mui/material/Button';
 
 function HeaderContent() {
   return (
-    <h1 style={styles.headerText}>Please help us!</h1>
+      <>
+        <h1 style={styles.headerText}>SIMPLE / BEAUTIFUL</h1>
+        <h2 style={styles.subtitle }>LANDING PAGE</h2>
+
+        <div style={styles.content}>
+            <p style={{ marginTop : -5 ,}}>Aliquip dolor culpa ad officia tempor eu id do reprehenderit. Ex dolore ea anim laboris ea nostrud quis labore aliquip laboris incididunt. Officia exercitation nostrud laboris aute quis. Reprehenderit non veniam mollit sint dolor proident ad. Ea laboris duis sint duis consectetur cillum commodo aliqua ullamco.</p><br/>
+            
+            <div style={{ width : '100%' , textAlign : 'center' , display : 'flex' , justifyContent : 'space-around' , alignItems : 'center' , marginTop : 15}}>
+                <Button style={{ color: "#21b6ae", border : '1px solid white' }}  variant="outlined">Outlined</Button>
+                <Button style={{ color: "#21b6ae", border : '1px solid white' }} variant="outlined">Outlined</Button>
+            </div>
+
+        </div>
+
+
+      </>
   )
 }
 
@@ -10,8 +26,25 @@ function HeaderContent() {
 const styles = {
 
     headerText : {
+        color : 'white',
         fontSize: '4em',
+        textAlign: 'center',
+    },
+
+    subtitle : {
+        color : 'white',
+        fontSize: '2em',
+        textAlign: 'center',
+        marginTop : -5
+    },
+
+    content : {
+        color : 'white',
+        justifyContent: 'center',
+        width : '50%',
+        textAlign: 'center',
     }
+
 
 }
 
